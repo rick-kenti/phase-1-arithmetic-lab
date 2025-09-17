84ea5f8660032e51425692c7b58fb2a1376cb8d0
rick-kenti <erickmakenti@gmail.com>
2025-09-17 04:59:15 +0300

    solved an arithmetic

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+const numbers = [10,20,5,];
+const max = Math.max(...numbers);
+
+module.exports = {num1, num2, multiply, random, num3, num4, mod, max};
